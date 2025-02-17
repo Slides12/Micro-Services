f281a5937dfc9f9b1ae826f5db64906c6328440e
Daniel Johansson <djohan93@hotmail.com>
2025-02-17 15:03:37 +0100

    Started Api-Gateway

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -22,7 +22,48 @@ if (app.Environment.IsDevelopment())
  });
 }
 
-app.MapGet("/", () => "Hello World!");
+// OrderService = 5189 (customer)
+// ProductService = 5141 (booking)
+
+var httpClient = new HttpClient();
+
+//Get all Customers
+app.MapGet("/gateway/GetAllCustomers", async () => {
+    return await httpClient.GetStringAsync("http://localhost:5189/GetAllCustomers");
+});
+//Get all Customer by id
+app.MapGet("/gateway/GetCustomerById/{id}", async (int id) => {
+    return await httpClient.GetStringAsync($"http://localhost:5189/GetCustomerById/{id}");
+});
+
+app.MapPost("/gateway/AddBooking/{object}", async (object) => {
+    return await httpClient.PostAsync($"http://localhost:5189/AddCustomer/{object}");
+});
+
+app.MapPut("/gateway/AddBooking/{object}", async (object, int id) => {
+    return await httpClient.PostAsync($"http://localhost:5189/AddCustomer/{object}");
+});
+
+
+//Get all bookings
+app.MapGet("/gateway/GetAllBookings", async () => {
+    return await httpClient.GetStringAsync("http://localhost:5141/GetAllBookings");
+});
+
+//Get all bookings
+app.MapGet("/gateway/GetAllBookings/{id}", async () => {
+    return await httpClient.GetStringAsync("http://localhost:5141/GetBookingById");
+});
+
+//Add bookings
+app.MapPost("/gateway/AddBooking/{object}", async (object) => {
+    return await httpClient.PostAsync($"http://localhost:5141/AddBooking/{object}");
+});
+
+//Delete booking
+app.MapDelete("/gateway/DeleteBooking/{id}", async (int id) => {
+    return await httpClient.DeleteAsync($"http://localhost:5141/DeleteBooking/{id}");
+});
 
 app.Run();
  
